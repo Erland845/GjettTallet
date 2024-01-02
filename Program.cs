41c896fe6194060bab75e50b8ea53fd8c5f6b508
Erland845 <141270802+Erland845@users.noreply.github.com>
2024-01-02 14:50:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace GjettTallet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            var randomNumber = rand.Next(0, 101);
+
+           while (true)
+            {
+                Console.WriteLine("Enter number");
+                var input = Console.ReadLine();
+                if (int.TryParse(input, out int userInput))
+                {
+                    if (userInput < randomNumber)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (userInput > randomNumber)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You won");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid number");
+                }
+            }
+        }
+    }
+}
